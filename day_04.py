ffe141e677193a326f52c9d4b9b070ded44247ad
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2026-07-19 00:01:51 +0200

    refactored code day 4

--- day_04.py
+++ day_04.py
@@ -1,45 +1,24 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 11 10:03:46 2020
-
-@author: laura
-"""
-
-
-# %% IMPORTS
 from hashlib import md5
 
-# %% DATA
-example1 = "abcdef"
-example2 = "609043"
-data = b"iwrupvqb"
+with open("./data/data_04.txt", encoding="utf-8") as f:
+    data = f.read().strip()
 
-# %% CALCULATIONS - Part 1
-example = example1 + example2
-test = md5(example.encode())
-print(test.hexdigest())
-
+# Part 1
 for i in range(1, 1000000):
     fullstr = data + str(i)
     hashed = md5(fullstr.encode())
     hashhex = hashed.hexdigest()
 
     if hashhex.startswith("00000"):
-        print("hashhex:", hashhex)
-        print("i:", i)
+        print(f"Part 1: {i}")
         break
 
-print("The answer is", i)
-
-# %% GOALS - Part 2
-"""Now find one that starts with six zeroes."""
-# %% CALCULATIONS - Part 2
+# Part 2
 for j in range(1, 100000000):
     fullstr = data + str(j)
     hashed = md5(fullstr.encode())
     hashhex = hashed.hexdigest()
 
     if hashhex.startswith("000000"):
-        print("hashhex:", hashhex)
-        print("The answer is", j)
+        print(f"Part 2: {j}")
         break
