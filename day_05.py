24f5412a06e82f94d20010ae65e28cbb7bdbd556
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2026-07-19 00:26:48 +0200

    refactored day 5

--- day_05.py
+++ day_05.py
@@ -1,74 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 11 18:59:37 2020
-
-@author: laura
-"""
-
-# %% GOALS - Part 1
-"""
-How many strings are nice?
-"""
-
-# %% IMPORTS
 import re
-import pandas as pd
-import numpy as np
 
+# DATA
+with open("./data/data_05.txt") as f:
+    data = f.read().splitlines()
 
-# %% FUNCTIONS  - Part 1
+# Part 1
 def FindVowelLength(string):
     num_vowels = len(re.findall("[aeiou]", string))
-
     return num_vowels
-
 
 def FindDoubleLetter(string):
     do_let = re.findall(r"(\w)(\1)", string)
-
-    if len(do_let) > 0:
-        result = "yes"
-    else:
-        result = "no"
-
-    return result
+    return len(do_let) > 0
 
 
 def ForbiddenCombinations(string):
     matches = re.findall(r"ab|cd|pq|xy", string)
-
-    if len(matches) > 0:
-        result = "yes"
-    else:
-        result = "no"
-
-    return result
+    return len(matches) > 0
 
 
-# %% DATA
-data = pd.read_csv("Data - Day05.csv", header=None)
+vowels = [FindVowelLength(i) for i in data]
+double_letter = [FindDoubleLetter(i) for i in data]
+forbidden_combos = [ForbiddenCombinations(i) for i in data]
 
-# %% CALCULATIONS - Part 1
-data["vowels"] = data[0].apply(FindVowelLength)
-data["double_letter"] = data[0].apply(FindDoubleLetter)
-data["forbidden_combos"] = data[0].apply(ForbiddenCombinations)
+nice_count = 0
+for i,j,k in zip(vowels, double_letter, forbidden_combos, strict=True):
+    if i >= 3 and j and not k:
+        nice_count += 1
 
-conditions = [
-    (data["vowels"] >= 3)
-    & (data["double_letter"] == "yes")
-    & (data["forbidden_combos"] == "no")
-]
+print(f"Part 1: {nice_count}")
 
-data["nice"] = np.select(conditions, ["yes"], default="no")
+# Part 2
+nice2 = 0
+for row in data:
+    if (re.search(r"([a-z])([a-z]).*(\1)(\2)", row) 
+        and re.search(r"([a-z])([a-z])(\1)", row)):
+            nice2 += 1
 
-print("Number of nice", len(data.loc[data["nice"] == "yes"]))
-
-
-# %% CALCULATIONS - Part 2
-nice = []
-for row in data[0]:
-    if re.search(r"([a-z])([a-z]).*(\1)(\2)", row):
-        if re.search(r"([a-z])([a-z])(\1)", row):
-            nice.append(row)
-
-print("Number of nice", len(nice))
+print(f"Part 2 {nice2}")
