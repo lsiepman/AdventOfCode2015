b5f85f0b49370091d80724bc4d4a43e29f1f4aa2
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2026-07-26 00:02:04 +0200

    another refactor of day 11 code

--- day_11.py
+++ day_11.py
@@ -1,113 +1,119 @@
 import re
 import string
 
-#  DATA
-with open('./data/data_11.txt') as f:
-    data = f.read().strip()
-# Part 1
-alphabet = string.ascii_lowercase
-data_nr = []
-for i in data:
-    data_nr.append(alphabet.index(i) + 1)
+def find_passwords(data, alphabet):  
+    data_nr = []
+    for i in data:
+        data_nr.append(alphabet.index(i) + 1)
 
-new_nr = []
-passnr = data_nr.copy()
-for i in range(3000000):
-    eight = passnr[7]
-    seven = passnr[6]
-    six = passnr[5]
-    five = passnr[4]
-    four = passnr[3]
-    three = passnr[2]
-    two = passnr[1]
-    one = passnr[0]
+    passwords = []
+    passnr = data_nr.copy()
+    while True:
+        eight = passnr[7]
+        seven = passnr[6]
+        six = passnr[5]
+        five = passnr[4]
+        four = passnr[3]
+        three = passnr[2]
+        two = passnr[1]
+        one = passnr[0]
 
-    if eight < 26:
-        eight += 1
-    elif seven < 26:
-        eight = 1
-        seven += 1
-    elif six < 26:
-        eight = 1
-        seven = 1
-        six += 1
-    elif five < 26:
-        eight = 1
-        seven = 1
-        six = 1
-        five += 1
-    elif four < 26:
-        eight = 1
-        seven = 1
-        six = 1
-        five = 1
-        four += 1
-    elif three < 26:
-        eight = 1
-        seven = 1
-        six = 1
-        five = 1
-        four = 1
-        three += 1
-    elif two < 26:
-        eight = 1
-        seven = 1
-        six = 1
-        five = 1
-        four = 1
-        three = 1
-        two += 1
-    elif one < 26:
-        eight = 1
-        seven = 1
-        six = 1
-        five = 1
-        four = 1
-        three = 1
-        two = 1
-        one += 1
-    else:
-        print("Error")
+        if eight < 26:
+            eight += 1
+        elif seven < 26:
+            eight = 1
+            seven += 1
+        elif six < 26:
+            eight = 1
+            seven = 1
+            six += 1
+        elif five < 26:
+            eight = 1
+            seven = 1
+            six = 1
+            five += 1
+        elif four < 26:
+            eight = 1
+            seven = 1
+            six = 1
+            five = 1
+            four += 1
+        elif three < 26:
+            eight = 1
+            seven = 1
+            six = 1
+            five = 1
+            four = 1
+            three += 1
+        elif two < 26:
+            eight = 1
+            seven = 1
+            six = 1
+            five = 1
+            four = 1
+            three = 1
+            two += 1
+        elif one < 26:
+            eight = 1
+            seven = 1
+            six = 1
+            five = 1
+            four = 1
+            three = 1
+            two = 1
+            one += 1
+        else:
+            print("Error")
 
-    passnr[7] = eight
-    passnr[6] = seven
-    passnr[5] = six
-    passnr[4] = five
-    passnr[3] = four
-    passnr[2] = three
-    passnr[1] = two
-    passnr[0] = one
+        passnr[7] = eight
+        passnr[6] = seven
+        passnr[5] = six
+        passnr[4] = five
+        passnr[3] = four
+        passnr[2] = three
+        passnr[1] = two
+        passnr[0] = one
 
-    passnr_new = passnr.copy()
+        passnr_new = passnr.copy()
+        word = create_word(passnr_new, alphabet)
+        if check_forbidden(word) and check_double(word) and check_combinations(word, alphabet):
+            passwords.append(word)
 
-    new_nr.append(passnr_new)
+        if len(passwords) == 2:
+            return passwords
 
-new_words = []
-for i in new_nr:
-    word = []
-    for j in i:
-        word.append(alphabet[j - 1])
-    new_words.append(word)
+def create_word(new_nr, alphabet):
+    word = ''
+    for j in new_nr:
+        word += alphabet[j - 1]
+    return word
 
-forbidden = ['i', 'l', 'o']
-first_selection = []
-for i in new_words:
-    if all(forbidden not in i for symbol in forbidden):
-        first_selection.append("".join(i))
+def check_forbidden(word, forbidden = ['i', 'l', 'o']):
+    if all(forbidden not in list(word) for symbol in forbidden):
+        return True
+    return False
 
-sec_selection = []
-for i in first_selection:
-    if len(re.findall(r"(\w)(\1)", i)) == 2:
-        sec_selection.append(i)
+def check_double(word):
+    if len(re.findall(r"(\w)(\1)", word)) == 2:
+        return True
+    return False
 
-combinations = []
-for i in range(24):
-    combinations.append(alphabet[i] + alphabet[i+1] + alphabet[i+2])
+def check_combinations(word, alphabet):
+    combinations = []
+    for i in range(24):
+        combinations.append(alphabet[i] + alphabet[i+1] + alphabet[i+2])
 
-three_selection = []
-for i in sec_selection:
-    if any(combi in i for combi in combinations):
-        three_selection.append(i)
+    if any(combi in word for combi in combinations):
+        return True
+    return False
 
-print(f"Part 1 {three_selection[0]}")
-print(f"Part 2 {three_selection[1]}")
+
+if __name__ == "__main__":
+    #  DATA
+    with open('./data/data_11.txt') as f:
+        data = f.read().strip()
+
+    alphabet = string.ascii_lowercase
+    passwords = find_passwords(data, alphabet)
+    print(f"Part 1 {passwords[0]}")
+    print(f"Part 2 {passwords[1]}")
