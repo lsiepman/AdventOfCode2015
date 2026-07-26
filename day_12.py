61f892e976d9123c39605f0b165b081f97d54b43
Laura Siepman <56088732+lsiepman@users.noreply.github.com>
2026-07-26 23:59:42 +0200

    refactored day 12 code

--- day_12.py
+++ day_12.py
@@ -1,47 +1,32 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 18 14:12:08 2020
-
-@author: laura
-"""
-
-# %% IMPORTS
 import json
 import re
-from itertools import chain
 
 
-# %% DATA
-with open("Data - Day12.json") as json_file:
-    text = json_file.read()
-
-with open("Data - Day12.json") as json_file:
-    json_data = json.load(json_file)
-
-
-# %% CALC 1
-numbers = re.findall(r"[-0-9]+", text)
-total = 0
-for i in numbers:
-    total = total + int(i)
-
-print("The sum of all numbers is", total)
-
-
-# %% CALC 2
 def Red(string):
     if "red" in string.values():
         return {}
     else:
         return string
+    
+# DATA
+with open("./data/data_12.txt") as json_file:
+    text = json_file.read()
+    json_data = json.loads(text)
+    cleaned_text = str(json.loads(text, object_hook=Red))
+
+# Part 1
+numbers = re.findall(r"[-0-9]+", text)
+total = 0
+for i in numbers:
+    total = total + int(i)
+
+print(f"Part 1: {total}")
 
 
-with open("Data - Day12.json") as json_file:
-    cleaned_text = str(json.load(json_file, object_hook=Red))
-
+# Part 2  
 cleaned_numbers = re.findall(r"[-0-9]+", cleaned_text)
 clean_calc = 0
 for i in cleaned_numbers:
     clean_calc = clean_calc + int(i)
 
-print("The sum off all non-red numbers is", clean_calc)
+print(f"Part 2: {clean_calc}")
